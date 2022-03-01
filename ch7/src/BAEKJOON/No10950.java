acb76dd14d90e3aacec1a63add34ec22ee643ae8
kikimGH <akita33@naver.com>
2022-03-01 20:29:58 +0900

    22.3.1

---
+++ ch7/src/BAEKJOON/No10950.java
@@ -0,0 +1,17 @@
+package BAEKJOON;
+import java.util.Scanner;
+public class No10950 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		for(int i=1;i<=num;i++) {
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int sum=a+b;
+			System.out.println(sum);
+			
+		}
+	}
+
+}
