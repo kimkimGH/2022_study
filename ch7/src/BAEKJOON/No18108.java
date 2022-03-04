9d576191edf6dc28c1538e5cba2d9ee0b1c94acc
kikimGH <akita33@naver.com>
2022-03-04 23:05:05 +0900

    22.3.4

---
+++ ch7/src/BAEKJOON/No18108.java
@@ -0,0 +1,11 @@
+package BAEKJOON;
+import java.util.Scanner;
+public class No18108 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=543;
+		System.out.println(n-k);
+	}
+}
