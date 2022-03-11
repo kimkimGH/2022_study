435c1bb3b7113048f3d3cf97f2f6506e8e281a0e
kikimGH <akita33@naver.com>
2022-03-11 23:21:12 +0900

    22.3.11

---
+++ ch7/src/codeUP/No1045.java
@@ -0,0 +1,18 @@
+package codeUP;
+import java.util.*;
+public class No1045 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("½ÇÇà");
+		int num=sc.nextInt();
+		int num1=sc.nextInt();
+		
+		System.out.println(num+num1);
+		System.out.println(num-num1);
+		System.out.println(num*num1);
+		System.out.println(num/num1);
+		System.out.println(num%num1);
+	}
+
+}
